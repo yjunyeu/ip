ad71e0621f0790dc0b63c06f40e017fdc0146f7c
yjunyeu <yapjunyeu109266@gmail.com>
2025-01-21 20:24:16 +0800

    Add the ability to store tasks and list tasks by the user

--- src/main/java/Waty.java
+++ src/main/java/Waty.java
@@ -1,8 +1,24 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Waty {
+    private static ArrayList<String> tasks = new ArrayList<>();
     private static String horizontalLineFormatter(String content) {
         String horizontalLine = "____________________________________________________________\n";
         return horizontalLine + content + horizontalLine;
+    }
+
+    private static String listTasks() {
+        StringBuilder listOfTasks = new StringBuilder();
+        for (int i = 0; i < tasks.size(); i++) {
+            String numberedTask = " " + String.valueOf(i + 1) + ". " + tasks.get(i);
+            listOfTasks.append(numberedTask);
+        }
+        return listOfTasks.toString();
+    }
+
+    private static String addTasks(String task) {
+        tasks.add(task);
+        return " added: " + task;
     }
     public static void main(String[] args) {
         String greet = horizontalLineFormatter(" Hello! I'm Waty\n" +
@@ -15,8 +31,13 @@ public class Waty {
             if (userInput.equals("bye\n")) {
                 System.out.println(bye);
                 break;
+            } else if (userInput.equals("list\n")) {
+                String list = horizontalLineFormatter(listTasks());
+                System.out.println(list);
+            } else {
+                String reply = horizontalLineFormatter(addTasks(userInput));
+                System.out.println(reply);
             }
-            System.out.println(horizontalLineFormatter(userInput));
         }
     }
 
