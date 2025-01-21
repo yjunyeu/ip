a45b98c6fe9acc33c80f3a46734fef8ff267a098
yjunyeu <yapjunyeu109266@gmail.com>
2025-01-21 19:30:21 +0800

    Add echo functionality to chatbot

--- src/main/java/Waty.java
+++ src/main/java/Waty.java
@@ -1,11 +1,23 @@
+import java.util.Scanner;
 public class Waty {
     public static void main(String[] args) {
-        String greet = "____________________________________________________________\n" +
+        String horizontalLine = "____________________________________________________________\n";
+        String greet = horizontalLine +
                 " Hello! I'm Waty\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
+                horizontalLine;
+        String bye = horizontalLine +
                 " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________";
+                horizontalLine;
         System.out.println(greet);
+        Scanner reader = new Scanner(System.in);
+        while (true) {
+            String userInput = reader.nextLine() + "\n";
+            if (userInput.equals("bye\n")) {
+                System.out.println(bye);
+                break;
+            }
+            System.out.println(horizontalLine + userInput + horizontalLine);
+        }
     }
 }
