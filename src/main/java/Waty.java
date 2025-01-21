bfc2958942d3a7d3282c7e0f2db52f4ce3ab10eb
yjunyeu <yapjunyeu109266@gmail.com>
2025-01-22 00:50:42 +0800

    Add the ability to mark tasks as done and also unmark tasks

--- src/main/java/Waty.java
+++ src/main/java/Waty.java
@@ -1,7 +1,12 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 public class Waty {
-    private static ArrayList<String> tasks = new ArrayList<>();
+    private static ArrayList<Task> tasks = new ArrayList<>();
+
+    private enum Command {
+        LIST, BYE, MARK, UNMARK;
+
+    }
     private static String horizontalLineFormatter(String content) {
         String horizontalLine = "____________________________________________________________\n";
         return horizontalLine + content + horizontalLine;
@@ -16,9 +21,20 @@ public class Waty {
         return listOfTasks.toString();
     }
 
-    private static String addTasks(String task) {
-        tasks.add(task);
-        return " added: " + task;
+    private static String addTasks(String taskDescription) {
+        Task newTask = new Task(taskDescription);
+        tasks.add(newTask);
+        return " added: " + taskDescription;
+    }
+
+    private static String markTask(int index) {
+        tasks.get(index).mark();
+        return " Nice! I've marked this task as done:\n " + tasks.get(index);
+    }
+
+    private static String unmarkTask(int index) {
+        tasks.get(index).unmark();
+        return " OK, I've marked this task as not done yet:\n " + tasks.get(index);
     }
     public static void main(String[] args) {
         String greet = horizontalLineFormatter(" Hello! I'm Waty\n" +
@@ -26,18 +42,35 @@ public class Waty {
         String bye = horizontalLineFormatter(" Bye. Hope to see you again soon!\n");
         System.out.println(greet);
         Scanner reader = new Scanner(System.in);
+
         while (true) {
             String userInput = reader.nextLine() + "\n";
-            if (userInput.equals("bye\n")) {
-                System.out.println(bye);
-                break;
-            } else if (userInput.equals("list\n")) {
-                String list = horizontalLineFormatter(listTasks());
-                System.out.println(list);
-            } else {
+            String[] split = userInput.split("\\s+");
+            try {
+                Command command = Command.valueOf(split[0].toUpperCase());
+
+                switch (command) {
+                    case BYE:
+                        System.out.println(bye);
+                        return;
+                    case LIST:
+                        String list = horizontalLineFormatter(listTasks());
+                        System.out.println(list);
+                        break;
+                    case MARK:
+                        int markIndex = Integer.parseInt(split[1]) - 1;
+                        System.out.println(markTask(markIndex));
+                        break;
+                    case UNMARK:
+                        int unmarkIndex = Integer.parseInt(split[1]) - 1;
+                        System.out.println(unmarkTask(unmarkIndex));
+                        break;
+                }
+            } catch (IllegalArgumentException e) {
                 String reply = horizontalLineFormatter(addTasks(userInput));
                 System.out.println(reply);
             }
+
         }
     }
 
