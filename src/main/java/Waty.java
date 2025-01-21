fee01224b7170952d6ac6313eda8a50a0c58db1b
yjunyeu <yapjunyeu109266@gmail.com>
2025-01-21 19:06:58 +0800

    Rename Duke.java to Waty.java

---
+++ src/main/java/Waty.java
@@ -0,0 +1,11 @@
+public class Waty {
+    public static void main(String[] args) {
+        String greet = "____________________________________________________________\n" +
+                " Hello! I'm Waty\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________";
+        System.out.println(greet);
+    }
+}
